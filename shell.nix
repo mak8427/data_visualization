bb5813c104b480c07a6700ceebed3e7293c5d6ce
ruuis <luiskret@gmail.com>
2025-05-27 11:33:15 +0200

    added environment and extended gitignore

---
+++ shell.nix
@@ -0,0 +1,32 @@
+let
+  # We pin to a specific nixpkgs commit for reproducibility.
+  # Last updated: 2024-04-29.
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+
+  python = pkgs.python311;
+  pythonWithPackages = python.withPackages (ps: with ps; [
+    jupyter
+    ipython
+    ipykernel
+
+    setuptools
+    virtualenv
+    pip
+    pyqt5
+
+    numpy
+    scipy
+    matplotlib
+    plotly
+    pandas
+  ]);
+
+in pkgs.mkShell rec {
+  venvDir = "./.venv";
+
+  buildInputs = [ pythonWithPackages ];
+
+  shellHook = ''
+    jupyter notebook --ip=127.0.0.1 --port 8888 --no-browser --allow-root --NotebookApp.token=\'\' --NotebookApp.password=\'\'
+  '';
+}
